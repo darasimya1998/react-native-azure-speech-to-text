cf33d2eced9c73c1d3cef12dc561eef284d5f2af
NEB DARA <83274622+darasimya1998@users.noreply.github.com>
2025-02-10 02:04:20 +0700

    fix: added dist to .gitignore

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-/**
- * @format
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AzureSTT = void 0;
-var AzureSTT_1 = require("./AzureSTT");
-Object.defineProperty(exports, "AzureSTT", { enumerable: true, get: function () { return AzureSTT_1.AzureSTT; } });
